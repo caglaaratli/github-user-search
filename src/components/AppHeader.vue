18deb958813d5bcd1ae429e1feec7d8bd2e157b8
Cagla aratli <101931062+caglaaratli@users.noreply.github.com>
2023-08-18 14:36:47 +0300

    fix : light theme colors

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,81 +1,81 @@
 <template>
-    <div class="header">
-        <div class="head" :class="darkMode ? 'dark' : 'light'" >devFinder</div>
-        <div class="light-dark-mode">
-          <button class="color-mode-btn" :class="darkMode ? 'dark' : 'light'" @click="toggleDarkMode" aria-label="Toogle light mode">
-            {{ darkMode ? 'LIGHT' : 'DARK' }}
-           <img  width="20" height="20" :src="darkMode ? require('@/assets/icon-sun.svg') : require('@/assets/icon-moon.svg')">
-          </button>
-          <!-- <button class="color-mode__btn dark--hidden" aria-label="Toggle dark mode">
-           DARK
-           <img  width="22" height="22" src="@/assets/icon-moon.svg">
-          </button> -->
+  <div class="header">
+      <div class="head" :class="darkMode ? 'dark' : 'light'" >devFinder</div>
+      <div class="light-dark-mode">
+        <button class="color-mode-btn" :class="darkMode ? 'dark' : 'light'"  @click="toggleDarkMode" aria-label="Toogle light mode">
+          {{ darkMode ? 'LIGHT' : 'DARK' }}
+         <img  width="20" height="20" :src="darkMode ? require('@/assets/icon-sun.svg') : require('@/assets/icon-moon.svg')">
+        </button>
+        <!-- <button class="color-mode__btn dark--hidden" aria-label="Toggle dark mode">
+         DARK
+         <img  width="22" height="22" src="@/assets/icon-moon.svg">
+        </button> -->
 
-        </div> 
-    </div>
+      </div> 
+  </div>
 
 </template>
 
 <script>
 export default {
-  props: {
-    darkMode: {
-        types:Boolean,
-        required:true, 
-        },
+props: {
+  darkMode: {
+      types:Boolean,
+      required:true, 
+      },
+},
+methods: {
+  toggleDarkMode() {
+    this.$emit('toggle-dark-mode'); // Ana bileşene dark mode durumu değiştiğini iletiyoruz
   },
-  methods: {
-    toggleDarkMode() {
-      this.$emit('toggle-dark-mode'); // Ana bileşene dark mode durumu değiştiğini iletiyoruz
-    },
-  },
+},
 };
 </script>
 
 <style >
 
 .head{
-  color:var( --head-color);
-  font-weight: bold;
-  font-size:26px;
+color:var( --head-color);
+font-weight: bold;
+font-size:26px;
 }
 .header{
-  display:flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 730px;
-  margin-bottom: 25px;
+display:flex;
+justify-content: space-between;
+align-items: center;
+width: 730px;
+margin-bottom: 25px;
 }
 
 
 .color-mode-btn{
-  font-size:13px;
-  outline:none;
-  background-color:var( --body-bg-color);
-  border:none;
-  cursor:pointer;
-  color:var(--darklight-color);
-  display: flex;
-  justify-content: space-between;
-  width:78px;
-  align-items: center;
-  
+font-size:13px;
+outline:none;
+background-color:var( --body-bg-color);
+border:none;
+cursor:pointer;
+color:var(--darklight-color);
+display: flex;
+justify-content: space-between;
+width:78px;
+align-items: center;
+
 
 }
 
 @media screen and (max-width:769px) {
-  .header{
-    width:574px;
-  }
+.header{
+  width:574px;
+}
 
 }
 
 @media screen and (max-width:415px) {
-  .header{
-    width:327px;
-    margin-left:10px;
-    height:60px;
-  }
+.header{
+  width:327px;
+  margin-left:10px;
+  height:60px;
+}
 
 }
 
